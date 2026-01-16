6bcb20cd41c1c3448a4e054bbf3341c46acce361
amoahsolomon <awahdodem1@gmailcom>
2026-01-15 19:35:11 -1200

    git add . git commit -m "Add Header, MainContent, and Footer components" git push origin main

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import WelcomeMessage from './components/WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,10 +17,13 @@ function App() {
     
   <div>
     <WelcomeMessage />
+     <Header />
+      <MainContent />
+      <Footer />
   </div>
 
     </>
-  )
+  );
 }
 
 export default App
