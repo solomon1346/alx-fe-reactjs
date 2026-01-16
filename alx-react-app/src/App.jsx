ef2b00993ad0db9fb88264eec711ad0f8f2991d5
amoahsolomon <awahdodem1@gmailcom>
2026-01-15 19:54:50 -1200

    git add . git commit -m "Add UserProfile component using props" git push origin main

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,8 +6,8 @@ import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
-
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +19,12 @@ function App() {
     <WelcomeMessage />
      <Header />
       <MainContent />
+      <UserProfile 
+  name="Alice" 
+  age="25" 
+  bio="Loves hiking and photography" 
+/>
+
       <Footer />
   </div>
 
