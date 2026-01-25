6724b9a1fb5a25bbcc0b803159f0e09423603d87
amoahsolomon <awahdodem1@gmailcom>
2026-01-24 20:46:28 -1200

    git add . git commit -m "Add recipe details, edit, and delete functionality with routing" git push origin main

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import useRecipeStore from '../recipeStore';
+import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
