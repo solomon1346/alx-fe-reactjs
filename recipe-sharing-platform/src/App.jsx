a9ad2abbe1c7d5ea105487a485b26df1fefab053
amoahsolomon <awahdodem1@gmailcom>
2026-02-13 22:41:25 -1200

    git add . git commit -m "Building the Home Page of the Recipe Sharing Platform" git push -u origin main

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import HomePage from "./components/HomePage";
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Recipe Sharing Platform 🍲
-      </h1>
-    </div>
-  )
+  return <HomePage />;
 }
 
-export default App
+export default App;
