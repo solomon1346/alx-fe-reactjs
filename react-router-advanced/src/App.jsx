c9c4caa85dca39524b5e22d0bd17e349b3d90e99
amoahsolomon <awahdodem1@gmailcom>
2026-02-22 09:35:53 -1200

    git add . git commit -m "Advanced Routing in React with React Router" git push -u origin main

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
